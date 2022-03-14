e70ebca47234cb563d837c5be8c32bab3adb4330
Yaredbabesha <88878083+Yaredbabesha@users.noreply.github.com>
2022-03-14 07:18:02 -0900

    My commit message

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{[A[C[A[C[C[C[C[C[C[C[C[C[C[C[C[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C)[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[A[A
+[A[C[C[C
+[B[A[A[C[C[Ca
+[C[C[C[C[[B
+[A[A[C[B[B[C[A[A[B[C                   [D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[B main - Entry point
+[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[ Return: Always 0 (success)
+[C[C[C[C[C[C[C[C[C *[B[Aint main(void)
+{
+printf("size of a char: %d byte(s)\n", sizeof(char));
+printf("size of a char: %d byte(s)\n", sizeof(char));[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cn int: %d byte(s)\n", sizeof(int))[C;
+printf("size of a long int: %d byte(s)\n", sizeof(long int));
+printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("size of a float: %d byte(s)\n",sizeof(float));[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D sizeof(float));
+return (0);
+}
+[A[C[
