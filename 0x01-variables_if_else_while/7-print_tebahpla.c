1518fe31f847127f008175166372cb37a0253b71
Yaredbabesha <88878083+Yaredbabesha@users.noreply.github.com>
2022-03-14 05:20:55 -0900

    My commit message

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+char alphabet;
+for (alphabet = 122; alphabet >=97; alphabet--)
+{
+putchar(alphabet);
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[;
+}
+putchar('\n');
+return (0);
+}
