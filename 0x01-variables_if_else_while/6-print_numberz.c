35950ccc380182154c6831b4eb531c6a5d1e159f
Yaredbabesha <88878083+Yaredbabesha@users.noreply.github.com>
2022-03-14 04:51:52 -0900

    My commit message

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+{[D[C
+putchar (i);
+}
+putchar ('\n');
+return (0);
+}
