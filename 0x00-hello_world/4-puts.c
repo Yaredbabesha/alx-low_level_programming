93269c26842f021d77cda5ad0f8941b8835ef6b3
Yaredbabesha <88878083+Yaredbabesha@users.noreply.github.com>
2022-03-14 06:45:30 -0900

    modified:   4-puts.c git commit -m "My commit message"

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success/correct)
+ * Return: 0 after printing the function
  */
 int main(void)
-{[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C 0 after printing the function[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[B[D[D[D[D[D[D[D[D[D[D
+{
 puts("\"programming is like building a mutilingual puzzle");
 return 0;
 }
