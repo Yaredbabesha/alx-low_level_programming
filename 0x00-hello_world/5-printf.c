b5cbac62e1c0852dd05b5cfbde7e1ab4874025ff
Yaredbabesha <88878083+Yaredbabesha@users.noreply.github.com>
2022-03-14 06:57:34 -0900

    My commit message

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 after printing the function
+ */
+int main(void)
+{[D[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Creturn 1 after program excution[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[A[D[D[D[D[D[D[D[D[D[D[DMy hello, printf task[B[B[B[D[D[D[D[D[D[D[D[D[D[D[B[D[D[D[D[D[D[D[D[D[D[D[D
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}[A[A[A[A[A[A[A[A[C[[[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[B[C[C[C[C[C[C[Return: return 1 after program excution[C[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[My hello, pmain - Entry point[B[B[B[B[B[B[B[D[D[D[D[D[D[D[B[D[D[D[D[D[D[D[D[D[D[D[D
