cde9b3ae334b9722dd129eefb2a88a4dd60deb02
Yaredbabesha <88878083+Yaredbabesha@users.noreply.github.com>
2022-03-14 06:43:00 -0900

    My commit message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C 0 after printing the function[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[B[D[D[D[D[D[D[D[D[D[D
+puts("\"programming is like building a mutilingual puzzle");
+return 0;
+}
