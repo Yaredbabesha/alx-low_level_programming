9d3f537e1a76d4b9ec2e1de8f5645875d4da8744
Yaredbabesha <88878083+Yaredbabesha@users.noreply.github.com>
2022-03-14 04:22:04 -0900

    modified:   ../0x01-variables_if_else_while/3-print_alphabets.c 	git commit -m "My commit message"

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[[A[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[B
+ *
  * Return: Always 0 (success/correct)
  */
 int main(void)
 {
 char lower_case;
 char upper_case;
-for (upper_case = 'A'; upper_case <= 'z'; upper_case++)
-{
-putchar(upper_case);
-}
-for (lower_case = 'a"; lower_case <= 'z'; lower_case++)
+for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
 putchar(lower_case);
 }
-putchar('\n');
+for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+{
+putchar(upper_case);
+}
+putchar ('\n');
 return (0);
 }
