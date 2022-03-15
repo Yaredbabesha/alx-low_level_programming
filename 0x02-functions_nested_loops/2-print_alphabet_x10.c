d1501a250062fcc2c5fe807fe6b27fe32bb93a99
Yaredbabesha <88878083+Yaredbabesha@users.noreply.github.com>
2022-03-15 08:29:40 -0900

    modified:   2-print_alphabet_x10.c git commit -m "My commit 	message"

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,25 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
-
-#include <stdarg.h>
+#include "holberton.h"
 /**
-  * print_alphabet_x10 - repeats the print_alphabet 10 times.
-  * Return: Nothing.
-  */
+ * print_alphabet_x10 - print the alphabet 10 times.
+ *
+ * Return: Always 0 (Success)
+ */
 void print_alphabet_x10(void)
 {
-     int i;
-     char c;
+	int alpha;
+	int count;
 
-     for (i = 0; i < 10; ++i)
-    {
-         for (c = 'a'; c <= 'z'; c++)
-           {
-              -putchar(c);
-           }
-             -putchar('\n');
-        }
+	count = 0;
+
+	while (count <= 9)
+	{
+		alpha = 'a';
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		count++;
+		_putchar('\n');
+	}
 }
