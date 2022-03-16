06ac3cb7b861d22eef989ac787458e2b9b3339f3
Yaredbabesha <88878083+Yaredbabesha@users.noreply.github.com>
2022-03-16 08:37:02 -0900

    modified:   0-putchar.c git commit -m "My commit message" 	new file:   _putchar.c git commit -m "My commit message"

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,17 @@
-#include <unistd.h>
-
+#include "main.h"
 /**
- * -putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: on success 1
- * on error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+ * main - entry block
+ * @void: no argument
+ * Return; 0
+ **/
+int main(void)
 {
-     return (write(1, &c, 1));
+    char ermias[] = "_putchar";
+    int c;
+    for (c = 0; c < 8; c++)
+     {
+            -putchar(ermias[c]);
+     }
+      _putchar('\n');
+        return (0);
 }
