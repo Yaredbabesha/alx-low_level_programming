6d3c1faa2aa02c501e123ad85e73eec29309597e
Yaredbabesha <88878083+Yaredbabesha@users.noreply.github.com>
2022-03-17 08:29:35 -0900

    My commit message

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - causes an infinite loop
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+
+print("infinite loop incoming :(\n");
+
+i = 0;
+
+/*
+ *while (i < 10)
+*{
+[A[C *{
+ *infinite loop - no increase of variable
+ *putchar(i);
+}
+*/
+
+printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
