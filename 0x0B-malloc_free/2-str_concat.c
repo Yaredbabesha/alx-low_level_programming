5ded82c5a739893e6627c5277984dcab65716b1f
Yaredbabesha <88878083+Yaredbabesha@users.noreply.github.com>
2022-04-04 06:10:38 -0900

    My commit message

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,33 +1,55 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "holberton.h"
 
 /**
- * _strlen - find length of a string
- * @s: string
- * Return: int
+ * _strlen - length of a string
+ * @s: input char
+ * Return: length of a string
  */
-
 
 int _strlen(char *s)
 {
-int size = 0;
-for (; s[size] != '\0'; size++)
-;
-return (size);
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		l++;
+	}
+	return (l);
 }
 
 /**
- * *str_concat - concatenates two strings
- * @s1: string 1
- * @s2: string 2
- * Return: pointer
- */
+* str_concat - Concat 2 strings.
+* @s1: string
+* @s2: string
+* Return: char
+*/
 
 char *str_concat(char *s1, char *s2)
 {
-int size1, size2, i;
-char *m;
+	unsigned int l1, l2;
+	char *conc, *tmp;
 
-if (s1 == NULL)
+	if (!s1)
+		s1 = "";
+	else
+		l1 = _strlen(s1);
 
+	if (!s2)
+		s2 = "";
+	else
+		l2 = _strlen(s2);
+
+	conc = malloc(l1 + l2 + 1);
+	if (!conc)
+		return (0);
+
+	tmp = conc;
+	while (*s1)
+		*tmp++ = *s1++;
+
+	while ((*tmp++ = *s2++))
+		;
+
+	return (conc);
+}
