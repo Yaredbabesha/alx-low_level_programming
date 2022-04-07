1e654ea1bfa43ed33438466dd6b6bc7ed1355747
Yaredbabesha <88878083+Yaredbabesha@users.noreply.github.com>
2022-04-07 11:31:00 -0900

    modified:   0-malloc_checked.c git commit -m "My commit message"

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,15 @@
 #include "holberton.h"
-
 /**
- * malloc_checked - allocates memory using malloc
- * @b: bytes allocate
- * Return: pointer
- */
+* malloc_checked - allocates memory
+* @b: n.o of bytes
+* Return: return a void ptr
+*/
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
 	ptr = malloc(b);
-	if (ptr == NULL)
+	if (!ptr)
 		exit(98);
 	return (ptr);
 }
