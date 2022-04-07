792545cac91dff9798311748a40a1a70d72552ef
Yaredbabesha <88878083+Yaredbabesha@users.noreply.github.com>
2022-04-07 11:28:09 -0900

    modified:   2-calloc.c git commit -m "My commit message"

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,26 +1,28 @@
-#include "main.h"
-
+#include <stdlib.h>
+#include "holberton.h"
+#include <string.h>
 /**
-* _calloc - allocate memory using malloc and initialize it to zero
-*@nmemb: number of elements
-*@size: size of the memory block to be allocated
-*
-*Return: poiner to the address of the memory block
-*/
+ *_calloc -allocates memory for an array
+ *@nmemb: number of element in the array
+ *@size: bytes for each position in the array
+ *Return: pointer void
+ */
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *block;
-	unsigned int i;
+	char *x;
+	unsigned int a;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	block = malloc(nmemb * size);
-	if (block != NULL)
-	{
-		for (i = 0; i < (nmemb * size); i++)
-			block[i] = 0;
-		return (block);
-	}
-	else
+
+	x = malloc(nmemb * size);
+	if (x == NULL)
 		return (NULL);
+
+	for (a = 0; a < (nmemb * size); a++)
+		x[a] = 0;
+
+	return (x);
+
 }
