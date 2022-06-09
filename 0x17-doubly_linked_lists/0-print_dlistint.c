ea558d5a5a832d6252dad564fa3a2c2f232c8d2c
Yaredbabesha <88878083+Yaredbabesha@users.noreply.github.com>
2022-06-09 12:04:52 -0900

    modified:   0-print_dlistint.c git commit -m "My commit message"

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,23 +1,20 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements of a dlistint_t list.
- * @h: pointer to head of the list
- * Return: number of nodes in the list
- **/
+ * print_dlistint - print elements of a doubly linked list
+ * @h: pointer to head of list
+ * Return: no of nodes
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t size;
+	const dlistint_t *current = h;
+	size_t size = 0;
 
-	size = 0;
-	if (h == NULL)
-	return (size);
-
-	while (h)
+	while (current)
 	{
-	printf("%d\n", h->n);
-	size++;
-	h = h->next;
+		printf("%i\n", current->n);
+		current = current->next;
+		size++;
 	}
 	return (size);
 }
